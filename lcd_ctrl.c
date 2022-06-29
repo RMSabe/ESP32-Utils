c4f7d7d40e0eea2461c1702f42f727170f99fe5f
RMSabe <69878824+RMSabe@users.noreply.github.com>
2022-06-29 12:24:29 -0300

    Update lcd_ctrl.c

--- lcd_ctrl.c
+++ lcd_ctrl.c
@@ -75,3 +75,83 @@ void lcd_init(lcd_t *lcd)
   
   return;
 }
+
+void lcd_set_display_mode(lcd_t *lcd, lcd_display_mode_t display_mode)
+{
+  switch(display_mode)
+  {
+    case LCD_DISPLAY_OFF:
+      lcd_send_byte(lcd, false, 0x08);
+      break;
+      
+    case LCD_CURSOR_OFF:
+      lcd_send_byte(lcd, false, 0x0C);
+      break;
+      
+    case LCD_CURSOR_ON:
+      lcd_send_byte(lcd, false, 0x0E);
+      break;
+      
+    case LCD_CURSOR_BLINK:
+      lcd_send_byte(lcd, false, 0x0F);
+      break;
+  }
+  
+  return;
+}
+
+void lcd_clear(lcd_t *lcd)
+{
+  lcd_send_byte(lcd, false, 0x01);
+  return;
+}
+
+void lcd_home(lcd_t *lcd)
+{
+  lcd_send_byte(lcd, false, 0x02);
+  return;
+}
+
+void lcd_set_cursor(lcd_t *lcd, uint8_t cx, uint8_t cy)
+{
+  if(cy >= lcd->n_lines) return;
+  
+  if(cy%2) lcd_send_byte(lcd, false, 0xC0);
+  else lcd_send_byte(lcd, false, 0x80);
+  
+  uint8_t nline = 0;
+  uint8_t nchar = 0;
+  
+  if(cy > 1)
+  {
+    for(nline = 0; nline < cy/2; nline++)
+      for(nchar = 0; nchar < lcd->n_char_per_line; nchar++)
+        lcd_send_byte(lcd, false, 0x14);
+  }
+  
+  if(cx > 0)
+  {
+    nchar = 0;
+    do{
+      lcd_send_byte(lcd, false, 0x14);
+      nchar++;
+    }while(nchar < cx);
+  }
+  
+  return;
+}
+
+void lcd_print_text(lcd_t *lcd, const char *text)
+{
+  uint32_t nlen = 0;
+  while(text[nlen] != '\0') nlen++;
+  
+  uint32_t nchar = 0;
+  while(nchar < nlen)
+  {
+    lcd_send_byte(lcd, true, text[nchar]);
+    nchar++;
+  }
+  
+  return;
+}
